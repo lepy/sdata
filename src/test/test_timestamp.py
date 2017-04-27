23e44d969b691a6d98393eec3a9853128b57e6e9
lepy <lep@lepy>
2017-04-27 09:20:09 +0200

    added test timestamp iso8601

--- src/test/test_timestamp.py
+++ src/test/test_timestamp.py
@@ -2,6 +2,8 @@ import time
 import datetime
 import os
 import pytz
+import iso8601
+
 
 def to_timestamp(dt, epoch=datetime.datetime(1970,1,1)):
     """2012-01-08 15:34:10.022403 -> 1326036850.02
@@ -29,24 +31,38 @@ def datetime2timestamp():
     # utctime = datetime.datetime.now(tz=pytz.UTC)
     print("UTC:", utctime.astimezone(tz=pytz.UTC).isoformat())
     print("CEST", utctime.astimezone(tz=pytz.timezone('Europe/Berlin')).isoformat())
+    print("CET ", utctime.astimezone(tz=pytz.timezone('CET')).isoformat())
 
     assert utctime.astimezone(tz=pytz.UTC).isoformat()=='2017-04-26T09:04:00.660000+00:00'
     assert utctime.astimezone(tz=pytz.timezone('Europe/Berlin')).isoformat()=='2017-04-26T11:04:00.660000+02:00'
 
-    print utctime.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
-    print utctime.strftime('%Y-%m-%d %H:%M:%S.%f %z')
-    print utctime.isoformat()
+    print(utctime.strftime('%Y-%m-%d %H:%M:%S.%f %Z'))
+    print(utctime.strftime('%Y-%m-%d %H:%M:%S.%f %z'))
+    print(utctime.isoformat())
 
     tz = pytz.timezone('Europe/Berlin')
     localtime = datetime.datetime.now(tz=tz)
-    print localtime.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
-    print localtime.strftime('%Y-%m-%d %H:%M:%S.%f%z')
-    print localtime.astimezone(tz=pytz.UTC).strftime('%Y-%m-%d %H:%M:%S.%f%z')
+    print(localtime.strftime('%Y-%m-%d %H:%M:%S.%f %Z'))
+    print(localtime.strftime('%Y-%m-%d %H:%M:%S.%f%z'))
+    print(localtime.astimezone(tz=pytz.UTC).strftime('%Y-%m-%d %H:%M:%S.%f%z'))
 
-    tzname = local_tzname()
-    print tzname
-    print localtime.astimezone(tz=pytz.timezone(tzname)).strftime('%Y-%m-%d %H:%M:%S.%f%z')
-    print localtime.astimezone(tz=pytz.timezone(tzname)).strftime('%Y-%m-%d %H:%M:%S.%f(%Z)')
+    # tzname = local_tzname()
+    # print tzname
+    tzname = "Etc/GMT-2"
+    localtime = utctime.astimezone(tz=pytz.timezone(tzname))
+    print("!", localtime.astimezone(tz=pytz.timezone(tzname)).isoformat())
+    # print localtime.astimezone(tz=pytz.timezone(tzname)).strftime('%Y-%m-%d %H:%M:%S.%f%z')
+    # print localtime.astimezone(tz=pytz.timezone(tzname)).strftime('%Y-%m-%d %H:%M:%S.%f(%Z)')
+    assert localtime.astimezone(tz=pytz.timezone(tzname)).isoformat()=="2017-04-26T11:04:00.660000+02:00"
+    print("UTC:", localtime.astimezone(tz=pytz.UTC).isoformat())
+    assert localtime.astimezone(tz=pytz.timezone("UTC")).isoformat()=="2017-04-26T09:04:00.660000+00:00"
+
+    timestr = "2017-04-26T09:04:00.660000+00:00"
+    time2 = iso8601.parse_date(timestr)
+    print(time2.astimezone(tz=pytz.timezone("UTC")).isoformat())
+    print(timestr)
+    assert timestr==time2.astimezone(tz=pytz.timezone("UTC")).isoformat()
+
 
 
 
