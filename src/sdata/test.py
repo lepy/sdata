c6817ecd887b2b50d2ce23c1a8c1367388ae38c6
gnulld <johannes20020304@gmail.com>
2017-05-12 13:34:05 +0200

    added test result

--- src/sdata/test.py
+++ src/sdata/test.py
@@ -5,6 +5,16 @@ class Test(sdata.Data):
     def __init__(self, **kwargs):
         """"""
         sdata.Data.__init__(self, **kwargs)
+        self._results = {}
+
+    def _get_results(self):
+        return self._results
+
+    def get_results(self, uuid):
+        return self._results.get(uuid)
+
+    def set_result(self, data):
+        self._results[data.uuid] = data
 
     def __str__(self):
         return "(Test '%s':%s)" % (self.name, self.uuid)
