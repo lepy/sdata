03c6dbf7cb871a3f1b95b7128e44236db31d668f
lepy <lep@lepy>
2017-05-16 14:22:08 +0200

    addad lapshear

--- src/sdata/experiments/__init__.py
+++ src/sdata/experiments/__init__.py
@@ -1,3 +1,4 @@
 from sdata.experiments.ks2 import KS2_Sheet, KS2_Test
+from sdata.experiments.lapshear import Lapshear_Sheet, Lapshear_Test
 
-__all__ = [KS2_Sheet, KS2_Test]
+__all__ = [KS2_Sheet, KS2_Test, Lapshear_Sheet, Lapshear_Test]
